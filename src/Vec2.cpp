0b1e970af25d09cbcc28305ea9b28615f366dd4e
Thomas Csigai <thomascsigai1@gmail.com>
2026-02-05 23:48:32 +0100

    Rename brVec2 into Vec2

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,47 +1,47 @@
-#include <brVec2.h>
+#include <Brise/Vec2.h>
 
 namespace Brise
 {
-	float& brVec2::operator[](int i)
+	float& Vec2::operator[](int i)
 	{
 		return ((&x)[i]);
 	}
 
-	const float& brVec2::operator[](int i) const
+	const float& Vec2::operator[](int i) const
 	{
 		return ((&x)[i]);
 	}
 
-	brVec2& brVec2::operator *=(float s)
+	Vec2& Vec2::operator *=(float s)
 	{
 		x *= s; y *= s;
 		return (*this);
 	}
 	
-	brVec2& brVec2::operator /=(float s)
+	Vec2& Vec2::operator /=(float s)
 	{
 		x /= s; y /= s;
 		return (*this);
 	}
 
-	brVec2& brVec2::operator +=(const brVec2& v)
+	Vec2& Vec2::operator +=(const Vec2& v)
 	{
 		x += v.x, y += v.y;
 		return (*this);
 	}
-	brVec2& brVec2::operator -=(const brVec2& v)
+	Vec2& Vec2::operator -=(const Vec2& v)
 	{
 		x -= v.x; y -= v.y;
 		return (*this);
 	}
 
-	std::ostream& operator<<(std::ostream& os, const brVec2& v)
+	std::ostream& operator<<(std::ostream& os, const Vec2& v)
 	{
 		std::cout << "(" << v[0] << ", " << v[1] << ")";
 		return os;
 	}
 	
-	std::string brVec2::ToString()
+	std::string Vec2::ToString()
 	{
 		std::ostringstream oss;
 		oss << "(" << x << ", " << y << ")";
