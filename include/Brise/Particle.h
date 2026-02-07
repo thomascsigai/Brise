cdf80f91673c238cfc80f9c16be906b6ae07c9b8
Thomas Csigai <thomascsigai1@gmail.com>
2026-02-07 13:45:59 +0100

    Adding particle interface/implementation

--- include/Brise/Particle.h
+++ include/Brise/Particle.h
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <Brise/Vec2.h>
+
 namespace Brise {
 	class Particle {
+
 	public:
-		Particle();
+
+		Vec2 position;
+		Vec2 velocity;
+		Vec2 acceleration;
+		
+	private:
+
+		float inverseMass;
+		float damping;
+
+	public:
+
+		Particle(Vec2 _pos, float mass, float damping);
+
+		void Integrate(float dt);
+		
+		void SetMass(float mass);
+		void SetInfiniteMass();
+		float GetMass();
+
 	};
 }
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,7 +1,47 @@
 #include <Brise/Particle.h>
+#include <Brise/BriseAssert.h>
+#include <limits>
 
 namespace Brise {
-	Particle::Particle() {
+	Particle::Particle(Vec2 position, float mass, float damping) 
+	: position(position), damping(damping) {
+		SetMass(mass);
+	}
 
+	/// <summary>
+	/// Integrates the particle forward in time by the given amount.
+	/// Using Newton Euler integration.
+	/// </summary>
+	/// <param name="duration">In ms</param>
+	void Particle::Integrate(float duration) {
+		// Don't integrate particles with infinite mass
+		if (inverseMass <= 0) return;
+
+		BR_ASSERT(duration > 0);
+
+		// Update Linear Position
+		position += velocity * duration;
+
+		// Update velocity from acceleration
+		velocity += acceleration * duration;
+
+		// Impose Drag
+		velocity *= pow(damping, duration);
+	}
+
+	void Particle::SetMass(float mass) {
+		BR_ASSERT(mass > 0);
+
+		inverseMass = 1 / mass;
+	}
+
+	void Particle::SetInfiniteMass() {
+		inverseMass = 0;
+	}
+
+	float Particle::GetMass() {
+		if (inverseMass == 0) return std::numeric_limits<float>::max();
+		
+		return (1.0f / inverseMass);
 	}
 }
