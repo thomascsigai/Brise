f3d4085a0550f59f7e73ade2ca55719bf095e03f
Thomas Csigai <thomascsigai1@gmail.com>
2026-02-08 20:52:43 +0100

    Add world class

---
+++ include/Brise/World.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Brise/Particle.h>
+#include <Brise/PForceGen.h>
+#include <Brise/Vec2.h>
+#include <vector>
+
+namespace Brise {
+
+	constexpr size_t DEFAULT_NUM_PARTICLES = 100;
+
+	class World {
+
+	public:
+		using ParticleContainer = std::vector<Particle>;
+
+	private:
+		ParticleContainer particles;
+		ParticleForceRegistry forceRegistry;
+		Vec2 gravity; // World gravity acceleration
+
+	public:
+
+		/// <param name="numParticles">: Should be above the number of particles your world will contains.
+		/// Works even if the number of particles exceeds this.</param>
+		explicit World(size_t numParticles = DEFAULT_NUM_PARTICLES);
+
+		void Step(float deltaTime);
+		void AddParticule(Vec2 position, float mass, float damping);
+		const ParticleContainer& GetParticles() const;
+
+	private:
+
+		void Init(size_t numParticles);
+		void Shutdown();
+
+		void SetGravity(Vec2 gravityForce);
+		Vec2 GetGravity();
+	};
+
+}
---
+++ src/World.cpp
@@ -0,0 +1,39 @@
+#include <Brise/World.h>
+
+namespace Brise {
+	World::World(size_t numParticles) {
+		Init(numParticles);
+	}
+
+	void World::Step(float deltaTime) {
+		forceRegistry.UpdateForces(deltaTime);
+
+		for (auto& p : particles) {
+			p.Integrate(deltaTime);
+		}
+	}
+
+	void World::AddParticule(Vec2 position, float mass, float damping) {
+		particles.push_back(Particle(position, mass, damping));
+		particles.back().acceleration = gravity; // Set world gravity as constant acceleration
+	}
+
+	const World::ParticleContainer& World::GetParticles() const {
+		return particles;
+	}
+
+	void World::Init(size_t numParticles) {
+		particles.reserve(numParticles);
+		SetGravity({ 0, -9.81 }); // Default to real world gravity acceleration
+	}
+
+	void World::Shutdown() {}
+
+	void World::SetGravity(Vec2 gravityForce) {
+		gravity = gravityForce;
+	}
+
+	Vec2 World::GetGravity() {
+		return gravity;
+	}
+}
